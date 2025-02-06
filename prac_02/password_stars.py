fc9c3d1209c22a6dc879b4280f9390cca20e78ef
airishlie <airishlie@gmail.com>
2025-02-06 17:31:52 +0800

    Add docstring to each function

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -8,10 +8,12 @@ def main() :
 
 
 def display_asteriks(password):
+    """Display asteriks equal to the length of the password"""
     print(password * "*")
 
 
 def get_password():
+    """Asks the user for a password and ensures that the length is not shorter than the password length"""
     password = len(input("Enter password : "))
     while password <= PASSWORD_LENGTH:
         print("Invalid Password")
