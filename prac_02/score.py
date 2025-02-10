1904309558cad5684253d060b2f291c265d4a696
airishlie <airishlie@gmail.com>
2025-02-10 09:29:23 +0800

    Change function name to a better function name and remove unnecessary function

--- prac_02/score.py
+++ prac_02/score.py
@@ -2,25 +2,35 @@ import random
 
 MINIMUM_SCORE = 0
 MAXIMUM_SCORE = 100
+EXCELLENT_THRESHOLD = 90
+PASS_THRESHOLD = 50
 
 def main ():
-    score = float(input("Enter score: "))
-    result = evaluate_score (score)
-    print(result)
+    """display grade"""
+    score = validate_score()
+    grade = get_grade(score)
+    print(grade)
+    random_number = random.uniform(MINIMUM_SCORE,MAXIMUM_SCORE)
+    #print random number
+    random_grade = get_grade(random_number)
+    print (random_grade)
 
-    random_score = random.randint(MINIMUM_SCORE,MAXIMUM_SCORE)
-    print(f"Random score: {random_score}")
-    print(evaluate_score(random_score))
-
-
-def evaluate_score (score) :
-    """Checks if the score is valid and display the result of the score"""
-    if score < MINIMUM_SCORE or score > MAXIMUM_SCORE:
-        return "Invalid score"
-    elif score >= 90:
-        return "Excellent"
-    elif  score >= 50:
-        return "Passable"
+def get_grade(score):
+    """Determine grade base on the score"""
+    if score >= EXCELLENT_THRESHOLD:
+        message = "Excellent"
+    elif score >= PASS_THRESHOLD:
+        message = "Passable"
     else :
-        return "Bad"
+        message = "Bad"
+    return message
+
+def validate_score () :
+    """Checks if the score is valid"""
+    score = float(input("Enter score: "))
+    while score < MINIMUM_SCORE or score > MAXIMUM_SCORE:
+        print ("Invalid score")
+        score = float(input("Enter score : "))
+    return score
+
 main()
