1fd2c627b87a4ce647bc9362bd59c0ae3c9177c8
airishlie <airishlie@gmail.com>
2025-02-17 09:25:37 +0800

    Better way to display result

--- prac_03/exceptions_to_complete.py
+++ prac_03/exceptions_to_complete.py
@@ -4,10 +4,11 @@ Fill in the TODOs to complete the task
 """
 
 is_finished = False
+result= ""
 while not is_finished:
     try:
         result = int(input("Enter a valid integer: "))
         is_finished = True
     except ValueError:
         print("Please enter a valid integer.")
-print("Valid result is:", result)
+print(f"Valid result is : result {result}")
