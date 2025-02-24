32b81d72ec17d602dad6d8b40923e2fd7c8e5ccc
airishlie <airishlie@gmail.com>
2025-02-24 12:59:59 +0800

    Change list name to better naming (data to subjects)

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -13,7 +13,7 @@ def main():
 
 def load_data():
     """Read data from file formatted like: subject,lecturer,number of students."""
-    data = []
+    subjects = []
     input_file = open(FILENAME)
     for line in input_file:
         print(line)  # See what a line looks like
@@ -23,10 +23,10 @@ def load_data():
         print(parts)  # See what the parts look like (notice the integer is a string)
         parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
         print(parts) # See if that worked
-        data.append(parts)
+        subjects.append(parts)
         print("----------")
     input_file.close()
-    return data
+    return subjects
 
 def display_subject_details(data):
     """Display subject details"""
