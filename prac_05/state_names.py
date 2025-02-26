9f33b4dd2251190fe6597b122064230a63c01624
airishlie <airishlie@gmail.com>
2025-02-26 22:21:01 +0800

    Complete practical 5 state names exercise

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -14,12 +14,13 @@ CODE_TO_NAME = {
     "SA": "South Australia"
 }
 
-print(CODE_TO_NAME)
+for code in CODE_TO_NAME :
+    print(f"{code:3} is {CODE_TO_NAME[code]}")
 
-state_code = input("Enter short state: ")
-while state_code != "":
-    if state_code in CODE_TO_NAME:
-        print(state_code, "is", CODE_TO_NAME[state_code])
-    else:
+state_code = input("Enter short state: ").upper()
+while state_code :
+    try :
+        print(f"{state_code} is {CODE_TO_NAME[state_code]}")
+    except KeyError :
         print("Invalid short state")
-    state_code = input("Enter short state: ")
+    state_code = input("Enter short state: ").upper()
