70c3d654200a719e9c5828461f4affa760a32d05
airishlie <airishlie@gmail.com>
2025-02-28 08:54:52 +0800

    Create program to store names with their email addresses. Include checks to ensure the email addresses are valid. Use dictionary to keep the information organized, and display output in a clear format.

---
+++ prac_05/emails.py
@@ -0,0 +1,28 @@
+def main() :
+    """Store emails and names in dictionary, allowing users to confirm their names"""
+    email_to_name = {}
+
+    email = input ("Email : ")
+    while email :
+        suggested_name = get_name_from_email(email)
+        confirmation = input(f"Is your name {suggested_name}? (Y/n) ").strip().lower()
+
+        if confirmation and confirmation != 'y':
+            name = input("Name: ").strip()
+        else :
+            name = suggested_name
+
+        email_to_name[email] = name
+        email = input ("Email: ")
+
+    print()
+    for email,name in email_to_name.items():
+        print(f"{name} ({email})")
+
+def get_name_from_email (email) :
+    """Extract a name from an email address"""
+    name_part = email.split ('@')[0]
+    name_parts = name_part.split('.')
+    return " ".join(name_parts).title()
+
+main()
