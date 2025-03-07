3771d0ae780d84456e75ba5bfad6f69c110fa9ee
airishlie <airishlie@gmail.com>
2025-03-07 11:01:15 +0800

    Improve test cases for Car class, including fuel addition and drive limits.

--- prac_06/used_cars.py
+++ prac_06/used_cars.py
@@ -10,9 +10,25 @@ from prac_06.car import Car
 def main():
     """Demo test code to show how to use car class."""
     my_car = Car(180)
+    print("Initial car state:", my_car)
+
     my_car.drive(30)
     print(f"Car has fuel: {my_car.fuel}")
-    print(my_car)
+    print("After driving 30 km", my_car)
+
+    # Adding fuel
+    my_car.add_fuel(20)
+    print ("After adding 20 fuel:", my_car)
+
+    # Driving further than available fuel
+    my_car.drive(200)
+    print ("After attempting to drive 200 km: ", my_car)
+
+    # Make sure that fuel not negative
+    print(f"Final fuel level: {my_car.fuel}")
+
+    # Test __str__()
+    print (f"Car string representation: {my_car}")
 
 
 main()
