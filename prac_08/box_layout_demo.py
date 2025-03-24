f6ad5bf08957c4756250914981e3f9b41afd958d
airishlie <airishlie@gmail.com>
2025-03-24 15:30:47 +0800

    Add handle_greet and handle_clear functions to control greeting and to clear the actions.

--- prac_08/box_layout_demo.py
+++ prac_08/box_layout_demo.py
@@ -8,5 +8,14 @@ class BoxLayoutDemo(App):
         self.root = Builder.load_file('box_layout.kv')
         return self.root
 
+    def handle_greet(self):
+        name = self.root.ids.input_name.text
+        self.root.ids.output_label.text = f"Hello {name}"
+
+    def handle_clear(self):
+        self.root.ids.input_name.text = ""
+        self.root.ids.output_label.text = ""
+
 
 BoxLayoutDemo().run()
+
