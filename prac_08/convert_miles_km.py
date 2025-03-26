e0230461197f2cef80c3f08010d86e584edae9c3
airishlie <airishlie@gmail.com>
2025-03-26 11:06:18 +0800

    Refactor miles to km converter: add StringProperty for dynamic output updates, handle invalid inputs, and remove manual convert button to auto-update on input changes

--- prac_08/convert_miles_km.py
+++ prac_08/convert_miles_km.py
@@ -6,6 +6,7 @@ Lindsay Ward, IT@JCU
 
 from kivy.app import App
 from kivy.lang import Builder
+from kivy.properties import StringProperty
 
 __author__ = 'Lindsay Ward'
 
@@ -13,33 +14,29 @@ MILES_TO_KM = 1.60934
 
 
 class MilesConverterApp(App):
-    """ MilesConverterApp is a Kivy App for converting miles to kilometres """
+    output_text = StringProperty()
+
     def build(self):
-        """ build the Kivy app from the kv file """
+        """Build the Kivy app from the kv file."""
         self.title = "Convert Miles to Kilometres"
-        self.root = Builder.load_file('convert_m_km_solution.kv')
+        self.root = Builder.load_file('convert_miles_km.kv')
+        self.handle_calculate()
         return self.root
 
     def handle_calculate(self):
-        """ handle calculation (could be button press or other call), output result to label widget """
+        """Handle calculation and update result in output label."""
         value = self.get_validated_miles()
         result = value * MILES_TO_KM
-        self.root.ids.output_label.text = str(result)
+        self.output_text = str(result)
 
     def handle_increment(self, change):
-        """
-        handle up/down button press, update the text input with new value, call calculation function
-        :param change: the amount to change
-        """
+        """Handle up or down button press and update input field and output."""
         value = self.get_validated_miles() + change
         self.root.ids.input_miles.text = str(value)
         self.handle_calculate()
 
     def get_validated_miles(self):
-        """
-        get text input from text entry widget, convert to float
-        :return: 0 if error, float version of text if valid
-        """
+        """Get text input, convert to float, or return 0 if invalid."""
         try:
             value = float(self.root.ids.input_miles.text)
             return value
