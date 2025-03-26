f7ae6db1467f6a0f543487f4f444286712e36eec
airishlie <airishlie@gmail.com>
2025-03-26 11:41:00 +0800

    Reverted previous commit. Initial commit mistakenly copied from kivy demos repository, which was not required for this task. Adjusted implementation to meet the correct test requirements.

--- prac_08/dynamic_labels.py
+++ prac_08/dynamic_labels.py
@@ -1,58 +1,35 @@
 """
 Kivy example for CP1404/CP5632, IT@JCU
-Dynamically create buttons based on content of dictionary
-Lindsay Ward, first version: 11/07/2016
+Dynamically create labels based on content of a list
+Lindsay Ward, adapted by Airish Yacob Lie, date: 26 March 2025
 """
 
 from kivy.app import App
 from kivy.lang import Builder
-from kivy.uix.button import Button
-from kivy.properties import StringProperty
-
-NEW_COLOUR = (1, 0, 0, 1)  # RGBA for red
-ALTERNATIVE_COLOUR = (1, 0, 1, 1)  # RGBA for magenta
+from kivy.uix.label import Label
 
 
-class DynamicWidgetsApp(App):
-    """Main program - Kivy app to demo dynamic widget creation."""
-    status_text = StringProperty()
+class DynamicLabelsApp(App):
+    """Main program - Kivy app to demo dynamic label creation."""
 
     def __init__(self, **kwargs):
         """Construct main app."""
         super().__init__(**kwargs)
-        # basic data (model) example - dictionary of names: phone numbers
-        self.name_to_phone = {"Bob Brown": "0414144411", "Cat Cyan": "0441411211", "Oren Ochre": "0432123456"}
+        # basic data (model) example - list of names
+        self.names = ["Bob Brown", "Cat Cyan", "Oren Ochre"]
 
     def build(self):
         """Build the Kivy GUI."""
-        self.title = "Dynamic Widgets"
-        self.root = Builder.load_file('dynamic_widgets.kv')
-        self.create_widgets()
+        self.title = "Dynamic Labels"
+        self.root = Builder.load_file('dynamic_labels.kv')
+        self.create_labels()
         return self.root
 
-    def create_widgets(self):
-        """Create buttons from data and add them to the GUI."""
-        for name in self.name_to_phone:
-            # create a button for each data entry, specifying the text
-            temp_button = Button(text=name)
-            temp_button.bind(on_press=self.press_entry)
-            # set the button's background colour
-            temp_button.background_color = NEW_COLOUR
-            # add the button to the "entries_box" layout widget
-            self.root.ids.entries_box.add_widget(temp_button)
-
-    def press_entry(self, instance):
-        """Handle pressing entry buttons."""
-        # get name (dictionary key) from the text of Button we clicked on
-        name = instance.text
-        # change the button's background colour
-        instance.background_color = ALTERNATIVE_COLOUR
-        # update status text
-        self.status_text = f"{name}'s number is {self.name_to_phone[name]}"
-
-    def clear_all(self):
-        """Clear all widgets that are children of the "entries_box" layout widget."""
-        self.root.ids.entries_box.clear_widgets()
+    def create_labels(self):
+        """Create labels from data and add them to the GUI."""
+        for name in self.names:
+            temp_label = Label(text=name)
+            self.root.ids.main.add_widget(temp_label)
 
 
-DynamicWidgetsApp().run()
+DynamicLabelsApp().run()
