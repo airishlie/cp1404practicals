fb641e98e5a42c0f85d3eb67c12b51ff6282bba6
airishlie <airishlie@gmail.com>
2025-04-13 13:47:34 +0800

    Fix Car() instantiation by adding missing name argument in tests

--- prac_10/testing.py
+++ prac_10/testing.py
@@ -43,28 +43,26 @@ def format_sentence(phrase):
 
 def run_tests():
     """Run the tests on the functions."""
-    # assert test with no message - used to see if the function works properly
+    # assert test with no message
     assert repeat_string("Python", 1) == "Python"
-    # the test below should now pass
     assert repeat_string("hi", 2) == "hi hi"
 
-    # assert test with custom message,
-    # used to see if Car's init method sets the odometer correctly
-    # this should pass (no output)
-    car = Car()
+    # assert test to check Car's odometer
+    car = Car("Test Car")
     assert car._odometer == 0, "Car does not set odometer correctly"
 
-    # assert tests to check fuel is set correctly
-    car_with_default_fuel = Car()
+    # assert tests to check Car's fuel handling
+    car_with_default_fuel = Car("Default Fuel Car")
     assert car_with_default_fuel.fuel == 0, "Default fuel is not 0"
 
-    car_with_fuel = Car(fuel=10)
+    car_with_fuel = Car("Fuel Car", fuel=10)
     assert car_with_fuel.fuel == 10, "Fuel not set correctly when given in constructor"
 
 
 run_tests()
 
-# Run the doctests
+# Run doctests
 doctest.testmod()
+
 
 
